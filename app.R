ea889fc9247157ab908af3d1bafb008106c79b33
Kevin Lynch <155801346+kdlynch55@users.noreply.github.com>
2025-10-01 14:23:05 -0500

    Update app.R
    Added Multiply and Add button

--- app.R
+++ app.R
@@ -9,20 +9,39 @@ ui <- fluidPage(
                          value = 0),
             numericInput(inputId = "number2",
                          label = "Number 2",
-                         value = 0)
+                         value = 0),
+            actionButton(inputId = "click",
+                         label = "Add"),
+            actionButton(inputId = "click_prod",
+                         label = "Multiply")
         ),
         mainPanel(
             h2("The sum of the two numbers is:"),
-            textOutput("sum")
+            textOutput("sum"),
+            h2("The result of multiplying the two numbers is:"),
+            textOutput("product")
         )
     )
 )
 
 server <- function(input, output) {
+    x2 <- eventReactive(input$click, {
+    as.numeric(input$number2)
+    })
+    x1 <- eventReactive(input$click, {
+    as.numeric(input$number1)
+    })
     output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
-        x1 + x2
+       x1() + x2()
+    })
+    x_2 <- eventReactive(input$click_prod, {
+      as.numeric(input$number2)
+    })
+    x_1 <- eventReactive(input$click_prod, {
+      as.numeric(input$number1)
+    })
+    output$product <- renderText({
+      x_1() * x_2()
     })
 }
 
